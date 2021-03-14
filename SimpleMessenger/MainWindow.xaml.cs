c8170edab470ea910e7fe8f45c0257300ca6f788
archyrysss <archyrysss@gmail.com>
2021-03-14 21:00:34 +0300

    Добавьте файлы проекта.

---
+++ SimpleMessenger/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using SimpleTCP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SimpleMessenger
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SimpleTcpServer server;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            server = new SimpleTcpServer();
+            server.Delimiter = 19;
+            server.StringEncoder = Encoding.UTF8;
+            server.DataReceived += DataReceived;
+        }
+
+        private void DataReceived(object sender, Message e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                TextSend.Text = e.MessageString;
+                server.BroadcastLine(TextSend.Text);
+            });   
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            IPAddress ip = IPAddress.Parse(TextHost.Text);
+            server.Start(ip, Convert.ToInt32(TextPort.Text));
+        }
+
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            if (server.IsStarted)
+            {
+                server.Stop();
+            }
+        }
+
+        private void Send_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ SimpleMessengerClient/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using SimpleTCP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SimpleMessengerClient
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SimpleTcpClient client;
+        private string nickName;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            client = new SimpleTcpClient();
+            client.StringEncoder = Encoding.UTF8;
+            client.DataReceived += DataReceived;
+        }
+
+        private void DataReceived(object sender, Message e)
+        {
+            Dispatcher.Invoke(() => TextMessages.Text += e.MessageString + "\r");
+        }
+
+        private void Send_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                client.WriteLineAndGetReply(nickName + ": " + TextMessageField.Text, TimeSpan.FromSeconds(3));
+            }
+            catch (Exception ex)
+            {
+                TextMessageField.Text = "Error";
+            }
+        }
+
+        private void Connect_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(TextName.Text))
+            {
+                TextMessageField.Text = "Connection failed. Type your nickname";
+                return;
+            }
+                
+                //client.WriteLineAndGetReply("From Client", TimeSpan.FromSeconds(3));
+            try
+            {
+                client.Connect(TextHost.Text, Convert.ToInt32(TextPort.Text));
+                nickName = TextName.Text;
+                TextMessageField.Text = "Connection Success";
+            }
+            catch (Exception ex)
+            {
+                TextMessageField.Text = "Connection failed";
+            }
+
+        }
+    }
+}
